b9c79d91da2ac7ab2fcc44537b26866aa7ae8f32
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2022-05-30 23:40:10 +0430

    working on different pages and styles

---
+++ interactive-rating-component-main/srcipt.js
@@ -0,0 +1,12 @@
+const submitBtn = document.getElementById('submitBtn');
+const btn1 = document.getElementById('btnradio1');
+const btn2 = document.getElementById('btnradio2');
+const btn3 = document.getElementById('btnradio3');
+const btn4 = document.getElementById('btnradio4');
+const btn5 = document.getElementById('btnradio5');
+
+
+submitBtn.addEventListener('click', function(e) {
+    console.log('gello')
+})
+
