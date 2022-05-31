d3298f9df40880d5337fc7111734d0f335b473cb
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2022-05-31 15:20:57 +0430

    some changes added

--- interactive-rating-component-main/srcipt.js
+++ interactive-rating-component-main/srcipt.js
@@ -1,19 +1,7 @@
-const submitBtn = document.getElementById('submitBtn');
-const btn1 = document.getElementById('btnradio1');
-const btn2 = document.getElementById('btnradio2');
-const btn3 = document.getElementById('btnradio3');
-const btn4 = document.getElementById('btnradio4');
-const btn5 = document.getElementById('btnradio5');
+let btnSubmit = document.getElementById('submitBtn');
+let result = document.getElementById('result-text');
 
-
-submitBtn.addEventListener('click', function(e) {
-    btn1Clicked();
+btnSubmit.addEventListener('click', function () {
+    let selected = document.querySelector('input[type="radio"]:checked').value;
+    result.innerHTML = `you have selected ${selected.value} out of 5`;
 })
-
-function btn1Clicked() {
-    btn1.checked = true;
-    btn2.checked = false;
-    btn3.checked = false;
-    btn4.checked = false;
-    btn5.checked = false;
-}
